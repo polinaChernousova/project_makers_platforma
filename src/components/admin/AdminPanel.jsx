cbf47f7fa866f63efb4d7e60db1aa191cb2e9bb4
posha <polinacernousova66@gmail.com>
2023-03-02 16:05:42 +0300

    added navbar admin-panel icon

---
+++ src/components/admin/AdminPanel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+
+const AdminPanel = () => {
+  return (
+    <div>
+      <AdminPanelSettingsIcon />
+    </div>
+  );
+};
+
+export default AdminPanel;
